af470dd47612e414aa50602335b89a7068de4035
Ceeessan <Cessan__94@hotmail.com>
2024-02-21 13:17:07 +0100

    test igen för att lösa problem

--- wp-content/themes/mytheme/checkout-hooks.php
+++ wp-content/themes/mytheme/checkout-hooks.php
@@ -13,3 +13,17 @@ function remove_order_review_heading() {
     return '';
 }
 
+<<<<<<< HEAD
+=======
+add_filter('woocommerce_checkout_fields', 'remove_billing_address_2_field');
+
+function remove_billing_address_2_field($fields) {
+    // Kontrollera om faktureringsadressen har ett andra radfält
+    if (isset($fields['billing']['billing_address_2'])) {
+        // Ta bort faktureringsadressens andra radfält
+        unset($fields['billing']['billing_address_2']);
+    }
+    return $fields;
+}
+
+>>>>>>> checkout
